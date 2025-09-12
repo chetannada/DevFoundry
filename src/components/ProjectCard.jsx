2a8090b18dcc77a3d04f497944e9020a0a8cd5b6
Chetan Nada <call2chetannada@gmail.com>
2025-09-13 00:32:05 +0530

    Project card item position changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,7 @@ import { FaGithub } from "react-icons/fa6";
 import { IoOpenOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { statusTooltips } from "../utils/constant";
 import { statusStyles } from "../utils/styles";
-import Tooltip from "./tooltip";
 
 const ProjectCard = ({
   item,
@@ -38,19 +36,17 @@ const ProjectCard = ({
   const toggleShowMore = () => setShowMore(!showMore);
 
   return (
-    <div className="group w-full flex flex-col flex-wrap gap-4 justify-between items-start px-6 py-4 bg-opacity-50 bg-purple-50 hover:scale-[1.02] transition-transform duration-300 hover:shadow-[0_10px_25px_-5px_rgba(139,92,246,0.5)] border border-gray-200 rounded-tr-3xl rounded-bl-3xl shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="group w-full flex flex-col flex-wrap gap-1 justify-between items-center p-5 bg-opacity-50 bg-purple-50 hover:scale-[1.02] transition-transform duration-300 hover:shadow-[0_10px_25px_-5px_rgba(139,92,246,0.5)] border border-gray-200 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700">
       <div className="w-full">
         {(contributorId === userId || userRole === "admin") && (
-          <div className="flex flex-wrap flex-row justify-between items-center gap-4 mb-3">
+          <div className="flex flex-wrap flex-row justify-between items-center gap-4 mb-2">
             {/* Edit/Delete Controls */}
 
             <div className="flex flex-wrap flex-row gap-3">
               {isDeleted ? (
-                <Tooltip text={"This project has been deleted"} width="w-36" left="left-1/3">
-                  <button className="text-xs px-3 py-1 bg-gray-300 text-gray-600 cursor-default rounded opacity-30 group-hover:opacity-100 transition-opacity duration-200">
-                    Deleted
-                  </button>
-                </Tooltip>
+                <button className="text-xs px-3 py-1 bg-gray-300 text-gray-600 cursor-default rounded opacity-30 group-hover:opacity-100 transition-opacity duration-200">
+                  Deleted
+                </button>
               ) : (
                 <button
                   onClick={() => handleEditModal(item)}
@@ -66,9 +62,7 @@ const ProjectCard = ({
               >
                 Delete
               </button>
-            </div>
 
-            <div className="flex flex-wrap flex-row gap-3">
               {userRole === "admin" && isDeleted && (
                 <button
                   onClick={() => handleRestoreModal(item)}
@@ -86,23 +80,23 @@ const ProjectCard = ({
                   Review
                 </button>
               )}
+            </div>
 
+            <div className="flex flex-wrap flex-row gap-3">
               {/* Status Badge */}
               {status && (contributorId === userId || userRole === "admin") && (
-                <Tooltip text={statusTooltips[status]} width="w-36" left="left-1/3">
-                  <span
-                    className={`px-3 py-1 text-xs font-semibold rounded-full ${statusStyles[status]} shadow-sm opacity-30 group-hover:opacity-100 transition-opacity duration-200`}
-                  >
-                    {status.charAt(0).toUpperCase() + status.slice(1).toLowerCase()}
-                  </span>
-                </Tooltip>
+                <span
+                  className={`px-3 py-1 text-xs font-semibold rounded-full ${statusStyles[status]} shadow-sm opacity-30 group-hover:opacity-100 transition-opacity duration-200`}
+                >
+                  {status.charAt(0).toUpperCase() + status.slice(1).toLowerCase()}
+                </span>
               )}
             </div>
           </div>
         )}
 
         {/* Title & Description */}
-        <div className={`flex flex-wrap flex-col gap-2 mb-5`}>
+        <div className={`flex flex-wrap flex-col gap-1 mb-2`}>
           <h5 className="text-2xl font-bold dark:text-white">{projectTitle}</h5>
           <p className="font-normal dark:text-gray-400">
             {showMore || projectDescription?.length <= characterLimit
@@ -121,7 +115,7 @@ const ProjectCard = ({
 
         {/* Tech Stack */}
         {Array.isArray(techStack) && techStack.length > 0 && (
-          <div className={`flex flex-wrap gap-2 mt-1 mb-6`}>
+          <div className={`flex flex-wrap gap-2 mb-3`}>
             {techStack.map((tech, index) => (
               <span
                 key={index}
@@ -135,56 +129,53 @@ const ProjectCard = ({
 
         {/* Rejection and Restored Reason */}
         {rejectionReason && (userId === contributorId || userRole === "admin") && (
-          <p className="w-fit max-w-full mb-6 p-2 text-sm text-red-700 bg-red-50 border border-red-200 rounded-md whitespace-pre-wrap break-words">
+          <p className="w-fit max-w-full mb-2 p-2 text-sm text-red-700 bg-red-50 border border-red-200 rounded-md whitespace-pre-wrap break-words">
             <strong>Rejected reason:</strong> {rejectionReason}
           </p>
         )}
         {restoredReason && (userId === contributorId || userRole === "admin") && (
-          <p className="w-fit max-w-full mb-6 p-2 text-sm text-green-700 bg-green-50 border border-green-200 rounded-md whitespace-pre-wrap break-words">
+          <p className="w-fit max-w-full mb-2 p-2 text-sm text-green-700 bg-green-50 border border-green-200 rounded-md whitespace-pre-wrap break-words">
             <strong>Restored reason:</strong> {restoredReason}
           </p>
         )}
       </div>
 
-      <div>
+      <div className="flex flex-wrap flex-row gap-4 justify-between items-center w-full">
+        {/* Contributor Info */}
+        <div className="text-sm flex items-center justify-center text-gray-600 dark:text-gray-400 opacity-90 group-hover:opacity-100 transition-opacity duration-200">
+          <Link
+            to={contributorGithubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="peer flex items-center gap-2 hover:scale-105 transition-transform duration-300"
+          >
+            <img
+              src={contributorAvatarUrl}
+              alt={`${contributorName}'s avatar`}
+              className="w-7 h-7 rounded-full border border-gray-300 dark:border-gray-600"
+            />
+            <span className="text-text-light dark:text-text-dark hover:underline">
+              {contributorName}
+            </span>
+          </Link>
+        </div>
+
         {/* Action Buttons */}
-        <div className="flex flex-wrap flex-row mob:flex-col gap-4 mob:gap-2 justify-between items-start mb-4">
+        <div className="flex flex-wrap flex-row gap-4 mob:gap-2 justify-start items-center">
           <Link
             to={githubCodeUrl}
             target="_blank"
-            className="flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-br from-fuchsia-500 to-blue-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+            className="min-w-24 flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-br from-fuchsia-500 to-blue-900 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm p-2"
           >
             <FaGithub size={20} /> Code
           </Link>
           <Link
             to={liveUrl}
             target={activeTab === "core" ? "_self" : "_blank"}
-            className="flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-r from-pink-500 to-purple-700 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5"
+            className="min-w-24 flex flex-row gap-2 justify-center items-center text-white bg-gradient-to-r from-pink-500 to-purple-700 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm p-2"
           >
-            <IoOpenOutline size={20} /> Live Demo
+            <IoOpenOutline size={20} /> Live
           </Link>
-        </div>
-
-        {/* Contributor Info */}
-        <div className="flex flex-wrap items-center gap-3 mt-2 text-sm italic text-gray-600 dark:text-gray-400 opacity-90 group-hover:opacity-100 transition-opacity duration-200">
-          <span className="dark:text-gray-300">Contributed by:</span>
-          <Tooltip text="View GitHub profile" width="w-36">
-            <Link
-              to={contributorGithubUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="peer flex items-center gap-2 hover:scale-105 transition-transform duration-300"
-            >
-              <img
-                src={contributorAvatarUrl}
-                alt={`${contributorName}'s avatar`}
-                className="w-7 h-7 rounded-full border border-gray-300 dark:border-gray-600"
-              />
-              <span className="text-blue-600 dark:text-blue-400 hover:underline">
-                {contributorName}
-              </span>
-            </Link>
-          </Tooltip>
         </div>
       </div>
     </div>
