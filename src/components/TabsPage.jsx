0b99ad98dbd7006ae5906d7d3467bb831948e8ec
Chetan Nada <call2chetannada@gmail.com>
2025-09-17 21:59:44 +0530

    Tabs value added in local storage for go back issue in different tab

--- src/components/TabsPage.jsx
+++ src/components/TabsPage.jsx
@@ -1,13 +1,18 @@
 import { MdAddCircleOutline } from "react-icons/md";
 
-const TabsPage = ({ activeTab, handleTabs, handleAddModal }) => {
+const TabsPage = ({ activeTab, setActiveTab, handleAddModal }) => {
+  const handleTabClick = tab => {
+    setActiveTab(tab);
+    localStorage.setItem("activeTab", tab);
+  };
+
   return (
     <div className="flex justify-between items-center mb-6 gap-4 flex-wrap">
       <div className="flex gap-2 p-1 rounded-xl bg-card-light dark:bg-card-dark border border-secondary-light dark:border-secondary-dark">
         {["core", "community"].map(tab => (
           <button
             key={tab}
-            onClick={() => handleTabs(tab)}
+            onClick={() => handleTabClick(tab)}
             className={`px-6 mob:px-3 py-3 rounded-xl text-sm  font-semibold transition-all duration-200
   ${activeTab === tab ? "text-white bg-gradient-to-br from-purple-500 to-blue-800 hover:bg-gradient-to-bl" : ""}`}
           >
