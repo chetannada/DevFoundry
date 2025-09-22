9a6e212abc9ee879bf494c2c1efcdcd015f39a36
Chetan Nada <call2chetannada@gmail.com>
2025-09-22 23:25:08 +0530

    feat: Image Gallery project added - modular, themed, and zoom-ready

---
+++ src/features/image-gallery/images.js
@@ -0,0 +1,26 @@
+export const images = [
+  {
+    original: "https://picsum.photos/id/37/1000/600/",
+    thumbnail: "https://picsum.photos/id/37/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/54/1000/600/",
+    thumbnail: "https://picsum.photos/id/54/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/74/1000/600/",
+    thumbnail: "https://picsum.photos/id/74/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/84/1000/600/",
+    thumbnail: "https://picsum.photos/id/84/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/71/1000/600/",
+    thumbnail: "https://picsum.photos/id/71/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/199/1000/600/",
+    thumbnail: "https://picsum.photos/id/199/250/150/",
+  },
+];
