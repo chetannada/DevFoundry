6a359c7bf363abe1c7115c01bcfdbc5e7514f943
Chetan Nada <call2chetannada@gmail.com>
2025-10-19 15:02:13 +0530

    Dev foundry logo and favicon added

---
+++ src/layout/Logo.jsx
@@ -0,0 +1,38 @@
+import { useTheme } from "../context/ThemeContext";
+import useWindowSize from "../hooks/useWindowSize";
+
+const Logo = () => {
+  const windowSize = useWindowSize();
+  const { theme } = useTheme();
+
+  const renderImage = () => {
+    return theme === "dark" ? (
+      <img
+        src="/public/images/devFoundry-logo-dark.png"
+        alt="DevFoundry Logo"
+        className="h-10 w-10 object-contain"
+      />
+    ) : (
+      <img
+        src="/public/images/devFoundry-logo-light.png"
+        alt="DevFoundry Logo"
+        className="h-10 w-10 object-contain"
+      />
+    );
+  };
+
+  return (
+    <>
+      {windowSize.width > 640 ? (
+        <h1 className="text-2xl max2xs:text-xl max3xs:text-base font-semibold tracking-widest">
+          <span className="text-secondary-light dark:text-secondary-dark">dev</span>
+          foundry
+        </h1>
+      ) : (
+        <>{renderImage()}</>
+      )}
+    </>
+  );
+};
+
+export default Logo;
